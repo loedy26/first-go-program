738adae15e8973d9f680e0e197f36e745d52f8bb
Loed Membreve <loedy26@gmail.com>
2020-10-26 22:29:11 +0800

    feat(main): add count 1-100

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	// exercise 1
 	fmt.Println("Hello World")
 	fmt.Println("Loed")
 	fmt.Println("G.")
 	fmt.Println("Membreve")
+
+	// exercise 2
+	for i := 1; i <= 100; i++ {
+		fmt.Println("Number", i)
+	}
 }
